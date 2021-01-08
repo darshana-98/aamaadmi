7ac159f7b66daaa79765299289fd5c895e854b78
darshana <darshanahareeshank@gmail.com>
2021-01-08 17:08:57 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,80 @@
-import Head from 'next/head'
+import React from 'react';
+import { FormControl, FormHelperText, InputLabel } from '@material-ui/core';
+import Head from 'next/head';
+import {makeStyles} from '@material-ui/core';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export default function Home() {
+
+const useStyle = makeStyles((theme)=>({
+  FormControl:{
+    margin:theme.spacing(1),
+    minWidth:140,
+  }
+}));
+
+
+
+function IndexPage() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+    <div className= "w-64 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600">
+      <div className="container">
+        <h1>
+      
+          <a>  "This is your fight"</a>
+      
         </h1>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <p className="text-purple-700 text-opacity-100 ...">Stay connected with India’s fastest growing party as it redefines the political landscape in India.</p>
+        <h2>Sign up now</h2>
+       
+      <FormControl className="bg-blue-500">
+    <input type="text" placeholder="Name*" ></input> 
+    </FormControl>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+    <FormControl>
+    <input type="text" placeholder="Mobile No"></input>
+    </FormControl>
+    <br></br>
+    <FormControl>
+      <input type="text" placeholder="Email Id"></input>
+      </FormControl>
+      <br></br>
+     
+      <FormControl>
+     <input type="text" placeholder="Pin Code"></input>
+      </FormControl>
+      <FormControl>
+      <button class="bg-red-900 ...">Sign Up</button>
+      </FormControl>
+      </div>
+    
+    <br></br>
+    <Image
+        src="/photo.jpg"
+        alt=""
+        width={425}
+        height={300}
+    
+       
+        />
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <button class="bg-green-800...">CONTINUE TO THE SITE>> </button>
+    <div className="image-column">
+    <Image
+        src="/pic3.png"
+        alt=""
+        width={100}
+        height={100}
+       
+        />
+        
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+         <h3>@2012-2017 Aam Aadmi Party.All Rights Reserved.</h3>
+    
     </div>
   )
 }
+
+export default IndexPage;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const { colors } = require("@material-ui/core")
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+
+  plugins: [],
+}
+module.exports = {
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'accent':'#333'
+       },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
