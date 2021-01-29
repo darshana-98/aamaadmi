b6c56b9ecb1609f345b7d86f5069b130736687fe
darshana <darshanahareeshank@gmail.com>
2021-01-29 14:33:31 +0530

    third commit

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { FormControl, FormHelperText, InputLabel } from '@material-ui/core';
 import Head from 'next/head';
 import {makeStyles} from '@material-ui/core';
-import Image from 'next/image';
-import Link from 'next/link';
 import { NoSchemaIntrospectionCustomRule } from 'graphql';
-import Counter from './Counter';
 
 
 
@@ -20,57 +17,71 @@ const useStyle = makeStyles((theme)=>({
 function IndexPage() {
   return (
 
-   <div>
-      <div className="container">
-
-        <h1>
-          <a>  SWEET MIXTURE 200 GM KOZHIKODANS</a>
-        </h1>
-        <h2>$2.99</h2>
+    <div className= "container">
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <p>33 in stock</p>
+        <button className="signin">Sign in</button>
+        <button className="close">Close</button>
+        <br></br>
+        <FormControl className="name">
+    <input  className="box1" type="text" placeholder="Username" ></input>
+    </FormControl>
+    <br></br>
+    <FormControl>
+    <input className="box1" type="text" placeholder="Password"></input>
+    </FormControl>
+    <br></br>
        
-  <Counter/>
-        <button className="new-btn">ADD TO CART</button>
-  <br></br>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-    <a class="active" href="#"><i class="fa fa-heart-o"></i>ADD TO WISHLIST</a> 
-    <a href="#"><i class="fa fa-share-alt"></i>SHARE</a> 
+   <div className="form">
+     <input  type="checkbox" name="remember"></input>
+     <label>Remember me</label>
+   </div>
+    <button className="new-btn" type="button">SIGN IN</button>
+    <br></br>
+    <div className="lost">
+    <a href="#">LOST YOUR PASSWORD?</a>
+  </div>
+  <style jsx>{`
  
-    </div>
-  
-    <Image
-        src="/sweet1.jpg"
-        alt=""
-        width={250}
-        height={450}
-    />
-    <style jsx>{`
-
-    .container{
-         
-  
-      padding: 50px;
-      padding-bottom: 100px;
-      max-width: 336px;
-      color: black;
-      float: right;
-      height:375px;
+     .container{
+       margin-left:40%;
+       margin-right:40%;
+   }
+    .lost{
+      margin-top: 40%;
+      margin-left: 15%;
     }
-    .new-btn{
-      color:white;
-      background-color:black;
-      width:150px;
-      height:50px;
-      margin-bottom:20%;
-      margin-left:50%;
-      margin-top:-5%;
+    .form{
+      margin-top:10%;
   }
-  
-  
+    .box1{
+      width:290px;
+      margin-top:5%;
+      border:8px solid rgba(0, 0, 0, 0.05);
+    };
     
-     `}</style>
+    }
+     .new-btn{
+       color:white;
+       background-color:black;
+       width:300px;
+       height:40px;
+       margin-top: 10%;
+   }
+   .signin{
+    background-color:white;
+    border:none;
+   }
+   .close{
+    background-color:white;
+    border:none;
+    margin-left: 60%;
+    margin-bottom:20%;
+   }
+     
+      `}</style>
     </div>
   )
 }
+
 export default IndexPage;
+
